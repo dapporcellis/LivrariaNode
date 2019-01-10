7def6493f2d82abd84a663ec3a6b0931750211ed
Diego de Abreu Porcellis <dapporcellis@hotmail.com>
2019-01-10 15:32:19 -0200

    a

--- config/passport.js
+++ config/passport.js
@@ -18,7 +18,7 @@ passport.use(new LocalStrategy({
   passwordField: 'txtSenha'
 },
   function (username, password, done) {
-    console.log(username)
+    //console.log(username)
     User.findOne({ email: username }, function (err, user) {
       if (err) throw err;
       if (!user) {
